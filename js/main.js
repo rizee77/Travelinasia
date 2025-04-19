ef1dfaf89200d25d1a19eab550f071fa16d4db12
Fakhr <rizefakhri@gmail.com>
2025-04-19 13:09:58 +0700

    update baru

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function changeNavbarBackground() {
     navbar.classList.remove("navbar-scrolled");
   }
 }
+
+// document.addEventListener('contextmenu', function(event) {
+//   event.preventDefault();
+// });
+
+// document.addEventListener('keydown', function(event) {
+//   //Cegah F12, Ctrl+Shift+I, dan Ctrl+U
+//   if (event.key === "F12" || 
+//       (event.ctrlKey && event.shiftKey && event.key === "I") || 
+//       (event.ctrlKey && event.key === "U")) {
+//     event.preventDefault();
+//   }
+// });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 1300){
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
